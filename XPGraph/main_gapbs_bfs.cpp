c0e2f27dc205ff69fbe1561add83d5a03656d7a1
LucasBastelli <lbspagnol@gmail.com>
2025-01-27 18:05:48 -0300

    Fixed bugs on Makefile and benchmarks

--- XPGraph/main_gapbs_bfs.cpp
+++ XPGraph/main_gapbs_bfs.cpp
@@ -5,8 +5,14 @@
 int main(int argc, const char ** argv) {
     XPGraph *xpgraph = new XPGraph(argc, argv);
     xpgraph->import_graph_by_config();
+    uint8_t count = xpgraph->get_query_count();
+    if (count == 0) return;
 
-    test_gapbs_bfs(xpgraph);
+    while (count--) {
+        test_gapbs_bfs(xpgraph);
+        std::cout << std::endl;
+    }
+    
 
     xpgraph->save_gragh();
     delete xpgraph;
